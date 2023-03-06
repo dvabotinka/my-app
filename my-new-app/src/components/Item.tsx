d298c0f02a03fa7c7db967430308fe8671be210a
StrdL <stas20001234567@mail.ru>
2023-03-06 23:33:00 +0200

    Add functionality of adding items

---
+++ my-new-app/src/components/Item.tsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import { Item } from "../store";
+import { observer } from "mobx-react";
+import 'antd/dist/reset.css';
+import { Table } from 'antd';
+
+interface ItemComponentProps {
+  item: Item
+}
+
+interface ItemComponentState {}
+
+@observer
+class ItemComponent extends Component<
+  ItemComponentProps, 
+  ItemComponentState> 
+  {
+  render() {
+    const { item } = this.props;
+
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        key: 'title',
+      },
+      {
+        title: 'Description',
+        dataIndex: 'description',
+        key: 'description',
+      }
+    ];
+
+    return (
+      <Table dataSource={[item]} columns={columns} pagination={false} />
+    )
+  }
+}
+
+export { ItemComponent };
