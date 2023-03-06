5c77b381f073318b523c25cc955c84073404a433
dvabotinka <126887449+dvabotinka@users.noreply.github.com>
2023-03-06 23:33:43 +0200

    Add functionality of adding items (#3)
    Co-authored-by: StrdL <stas20001234567@mail.ru>

--- my-new-app/src/App.tsx
+++ my-new-app/src/App.tsx
@@ -1,14 +1,40 @@
 import './App.css';
-import Roster from './Roster';
+import { Component } from "react";
+import { setupRootStore } from "./store/setup";
+import { Provider } from "mobx-react";
+import { MenuComponent } from './components/Menu';
 
-function App() {
-  return (
-    <div className="App">
+interface Props {}
+
+interface State {
+  rootTree: any
+}
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      rootTree: null
+    };
+  }
+  componentDidMount = () => {
+    const { rootTree } = setupRootStore();
+    this.setState({ rootTree });
+  };
+  render() {
+    const { rootTree } = this.state;
+    if (!rootTree) return null;
+    return (
+      <div className="App">
       <header className="App-header">
-        <Roster/>
+      <Provider rootTree={rootTree}>
+        <MenuComponent/>
+      </Provider>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
