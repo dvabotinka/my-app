c17ec27602bec2ed4092b3ec463cce24aae849eb
dvabotinka <126887449+dvabotinka@users.noreply.github.com>
2023-03-11 20:46:50 +0200

    Added functionality of saving data to localStorage (#5)
    Co-authored-by: StrdL <stas20001234567@mail.ru>

--- my-new-app/src/components/menu/Menu.tsx
+++ my-new-app/src/components/menu/Menu.tsx
@@ -1,13 +1,14 @@
 import { inject, observer } from "mobx-react";
 import { Component } from "react";
-import { Root } from "../../store/menu";
+import { Item, Root } from "../../store/menu";
 import 'antd/dist/reset.css';
 import { 
   Button, 
   Input, 
   Form,
   message,
-  Table
+  Table,
+  Spin
 } from 'antd';
 
 interface MenuComponentProps {
@@ -18,6 +19,8 @@ interface MenuComponentState {
   itemName: string,
   itemTitle: string,
   itemDescription: string,
+  menuData: Item[],
+  loading: boolean
 }
 
 @inject("rootTree")
@@ -32,8 +35,33 @@ class MenuComponent extends Component<
     this.state = {
       itemName: '',
       itemTitle: '',
-      itemDescription: ''
+      itemDescription: '',
+      menuData: [],
+      loading: true
     };
+  }
+
+  async componentDidMount() {
+    const { rootTree } = this.props;
+    if (!rootTree) return <Spin size="large" />;
+    
+    this.setState({ loading: true });
+
+    const items = await this.getItemsFromLocalStorage();
+    rootTree.menu.setItems(items);
+    this.setState({ menuData: [...rootTree.menu.items] });
+
+    this.setState({ loading: false });
+  }
+
+  async getItemsFromLocalStorage(): Promise<Item[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const menu = localStorage.getItem('Menu');
+        const items = menu ? JSON.parse(menu) : [];
+        resolve(items);
+      }, 2000);
+    });
   }
 
   changeItemName = (e: any) => {
@@ -60,6 +88,10 @@ class MenuComponent extends Component<
     if(!rootTree) return null;
 
     rootTree.menu.newItem(itemName, itemTitle, itemDescription);
+    
+    this.setState({ menuData: [...rootTree.menu.items] });
+
+    localStorage.setItem('Menu', JSON.stringify([...rootTree.menu.items]));
 
     setTimeout(() => {
       message.success('Item was added');
@@ -90,7 +122,7 @@ class MenuComponent extends Component<
       }
     ];
 
-    const dataSource = rootTree.menu.items.map((item) => ({
+    const dataSource = this.state.menuData.map((item) => ({
       id: item.id,
       name: item.name,
       title: item.title,
@@ -99,6 +131,7 @@ class MenuComponent extends Component<
 
     return (
       <div>
+        <Spin size="large" spinning={this.state.loading}></Spin>
         <h1>Menu</h1>
         <Form 
           color="red"
